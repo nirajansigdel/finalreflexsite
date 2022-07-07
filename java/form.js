1edc78e32b1c4909d5bf879f318927b563fd8098
Nirajan <suzalsigdel@gmail.com>
2022-07-07 15:13:59 +0545

    complete

--- java/form.js
+++ java/form.js
@@ -1,46 +1,30 @@
+alertify.set('notifier','position', 'top-right');   
    function validation(){
     var user=document.getElementById('user').value;
    var address=document.getElementById('address').value;
      var contact=document.getElementById('contact').value;
     var email=document.getElementById('email').value;
 
-      if(user==""){
-       document.getElementById('username').innerHTML="**please enter your name";
-      return false;
-    }
-
     if((user.length<=2) || (user.length>40)){
-        document.getElementById('username').innerHTML="**Name should be between 2-40 charcter";
+        alertify.error("**Name should be between 2-40 charcter");
         return false;
     }
     if(!isNaN(user))
     {
-        document.getElementById('username').innerHTML="**Name should be character";
-        return false;  
+    alertify.error("**Name should be character");
+    return false;  
     }
-    if(address==""){
-        document.getElementById('addressspan').innerHTML="**please enter your address";
-         return false;
-     }
-   
-     if(contact==""){
-        document.getElementById('contactspan').innerHTML="**please enter your contact number";
-     return false;
-     }
      if(isNaN(contact)){
-      document.getElementById('contactspan').innerHTML="**number should be  on digit only";
+        alertify.error("**number should be  on digit only");
    return false;
    }
    if(contact.length<=7){
-    document.getElementById('contactspan').innerHTML="**number must be greater than 7digit";
+    alertify.error("**number must be greater than 7digit");
      return false;
  }
-     if(email==""){
-        document.getElementById('emailspan').innerHTML="**please entr your email address";
-     return false;
-     }
+   
      if(email.indexof('@')<=0){
-      document.getElementById('emailspan').innerHTML="@should be after character";
+        alertify.error("@ should be after character");
    return false;
    }
 }
